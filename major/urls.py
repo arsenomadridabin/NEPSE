e28887562eabc94373c45c185c42574400556350
arsenomadridabin <arsenomadridabin@hotmail.com>
2017-07-22 16:41:33 +0545

    Everything added till live data

---
+++ major/urls.py
@@ -0,0 +1,25 @@
+"""major URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', 'stock.views.home', name='home1'),
+    url(r'^data/$', 'stock.views.data', name ='show_data'),
+    url(r'^updatedata/$', 'stock.views.update_data', name ='update_data'),
+    url(r'^updatedatadaily/$', 'stock.views.update_data_daily', name ='update_data_daily'),    
+]
